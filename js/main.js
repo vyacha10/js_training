0417da0232db246ca12853bbb582059df7ef2c26
vyacheslav sergeevich <slava.doroshenko.1998@mail.ru>
2023-09-07 16:45:39 +0500

    домашнее задание

---
+++ js/main.js
@@ -0,0 +1,19 @@
+alert ('Привет Capitan_Chewie');
+
+let money =1
+console.log ('money: ' , money);
+
+let income =2
+console.log ('income: ' , income);
+
+let addExpenses =3
+console.log ('adaddExpenses: ' ,addExpenses);
+
+let deposit =4
+console.log ('dedeposit: ' ,deposit);
+
+let mission =5
+console.log ('mission: ' , mission);
+
+let period =6
+console.log ('period: ' , period);
