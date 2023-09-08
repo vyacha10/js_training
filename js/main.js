484931ac616bbe04c09e53205bab0ccf5b005b6e
vyacheslav sergeevich <slava.doroshenko.1998@mail.ru>
2023-09-08 21:21:48 +0500

    закомител второй урок с домашней работой

---
+++ js/main.js
@@ -0,0 +1,42 @@
+let money =100000;
+console.log (typeof money);
+
+let income ='freelance';
+console.log ( typeof 'income');
+
+let addExpenses = 'internet, taxi, apartment';
+console.log (typeof 'addExpenses');
+console.log (addExpenses.length);
+console.log (addExpenses.toLowerCase());
+console.log (addExpenses.split (', '));
+
+let deposit =5>3;
+console.log (5>3);
+
+
+let mission = 'Goal to earn 1000000000$';
+console.log (mission, 'mission' ); //так получаю строку в console
+//let mission =1000000000;
+//console.log (typeof  mission); - так получаю nuber (число) как и просили из пункта 1
+
+let period ='The period is 12 months';
+console.log (period, 'period');
+console.log (typeof period); //вот так получаю строку в console
+//let period =12; 
+//console.log (typeof period); - так получаю nuber (число) как и просили из пункта 1
+
+
+
+let budgetDay =3333;
+console.log (budgetDay);
+
+
+//усложненное дз!
+const num =266219;
+console.log (typeof num);
+console.log (2*6*6*2*1*9);
+let a =1296;
+let b =3;
+console.log(a**b);
+let d ='2176782336';
+console.log(d.substr (0, 2));
