5f89b95bb4f1379978dc067c363a4f5c575ad0d2
vyacheslav sergeevich <slava.doroshenko.1998@mail.ru>
2023-09-12 23:36:18 +0500

    закомител исправленную работу

--- js/main.js
+++ js/main.js
@@ -14,16 +14,15 @@ let deposit =5>3;
 console.log (5>3);
 
 
-let mission = 'Goal to earn 1000000000$';
-console.log (mission, 'mission' ); //так получаю строку в console
-//let mission =1000000000;
-//console.log (typeof  mission); - так получаю nuber (число) как и просили из пункта 1
+let mission =  1000000000;
+const missionString = `цель заработать ${mission} долларов`;
+console.log (missionString); 
 
-let period ='The period is 12 months';
-console.log (period, 'period');
-console.log (typeof period); //вот так получаю строку в console
-//let period =12; 
-//console.log (typeof period); - так получаю nuber (число) как и просили из пункта 1
+
+let period =12;
+const periodString = `период равен ${period}`; 
+console.log(periodString);
+
 
 
 
@@ -37,6 +36,8 @@ console.log (typeof num);
 console.log (2*6*6*2*1*9);
 let a =1296;
 let b =3;
+const c = a**b;
 console.log(a**b);
-let d ='2176782336';
-console.log(d.substr (0, 2));
+let d = c.toString();
+
+console.log(d.slice(0, 2));
