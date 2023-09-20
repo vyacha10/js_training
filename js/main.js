01d673ad825c1799a2e54cd2d978fb0cd286b5a1
vyacheslav sergeevich <slava.doroshenko.1998@mail.ru>
2023-09-20 20:27:27 +0500

    закомител 3й урок с рно

--- js/main.js
+++ js/main.js
@@ -1,30 +1,37 @@
-let money = +prompt ('Ваш месячный доход?');
-console.log (money);
+const promptText = 'Ваш месячный доход?';
+const money = prompt(promptText);
+console.log(money);
+
+
 
 //let income ='freelance';
 //console.log ( typeof 'income');
-
-let addExpenses = prompt ('Перечислите возможные расходы за рассчитываемый период через запятую');
-console.log (addExpenses);
+let promptText2 = 'Перечислите возможные расходы за рассчитываемый период через запятую';
+let addExpenses = prompt(promptText2);
+console.log(addExpenses);
 //console.log (addExpenses.length);
 //console.log (addExpenses.toLowerCase());
 //console.log (addExpenses.split (', '));
+let confirmText = 'Есть ли у вас депозит в банке?'
+let deposit = confirm(confirmText);
+console.log(deposit);
 
-let deposit = confirm ('Есть ли у вас депозит в банке?');
-console.log (deposit);
-
-let expenses1 = prompt ('Введите обязательную статью расходов?');
-console.log (expenses1);
-let expenses2 = prompt ('Введите обязательную статью расходов?');
-console.log (expenses2);
-let amount1 = prompt ('Во сколько это обойдется?');
-console.log (amount1);
-let amount2 = prompt ('Во сколько это обойдется?');
-console.log (amount2);
+let promptText3 = 'Введите обязательную статью расходов?';
+let expenses1 = prompt(promptText3);
+console.log(expenses1);
+let promptText4 = 'Введите обязательную статью расходов?';
+let expenses2 = prompt(promptText4);
+console.log(expenses2);
+let promptText5 = 'Во сколько это обойдется?';
+let amount1 = prompt(promptText5);
+console.log(amount1);
+let promptText6 = 'Во сколько это обойдется?';
+let amount2 = prompt(promptText6);
+console.log(amount2);
 
 let budgetMonth = money - (amount1 + amount2);
 console.log('Бюджет на месяц: ', budgetMonth);
-let budgetMonth1 = Number(money) - (Number(amount1) + Number(amount2));
+let budgetMonth1 = +money - (+amount1 + +amount2);
 
 let mission =  1000000000;
 console.log(`Цель будет достигнута за: ${Math.ceil (mission/budgetMonth)} месяцев`);
@@ -34,13 +41,13 @@ let budgetDay = budgetMonth / 30;
 console.log('Бюджет на день: ', Math.floor(budgetDay));
 
 if (budgetDay > 1200) {
-    console.log ('у вас высокий уровень дохода');
+    console.log('у вас высокий уровень дохода');
 } else if (budgetDay > 600 && budgetDay < 1200 ) {
-    console.log ('у вас средний уровень дохода');
+    console.log('у вас средний уровень дохода');
 } else if (budgetDay < 600) {
-    console.log ('к сожалению уровень дохода ниже среднего');
+    console.log('к сожалению уровень дохода ниже среднего');
 } else if (budgetDay < 0) {
-    console.log ('что то  пошло не так')
+    console.log('что то  пошло не так')
 };
 
 //let period =12;
